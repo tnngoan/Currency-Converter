a04bbff2f53c79acc88b97b3f5657b45e6bd9de5
andrecee.tnn <andrecee.tnn@gmail.com>
2020-05-11 15:31:08 +0700

    submit

---
+++ main.js
@@ -0,0 +1,25 @@
+let amount = prompt("Input amount!");
+
+let from = prompt("From?");
+
+let to = prompt("to?");
+
+let currencyRatio = 23161.39;
+
+result = 0;
+
+if (from === "usd" && to === "vnd") {
+  result = usdToVnd();
+} else if (from === "vnd" && to === "usd") {
+  result = vndToUsd();
+} else result = 'Wrong type!';
+
+function usdToVnd() {
+  return amount * currencyRatio;
+}
+
+function vndToUsd() {
+  return amount / currencyRatio;
+}
+
+console.log(from, " to ", to, "is ", result);
